74c5634a0c35342707c2edf1c8d1009322a58c27
studentAutomations <student.automations@gmail.com>
2024-12-04 22:08:53 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -18,7 +18,7 @@ for url in WEBHOOK_URL:
     ) 
     
     # Dodaj stavke u embed
-    embed.add_field(name="**📢 Ažuriranje bota**", value="**Evo najnovijih promena:\n\n- Popravljeni su bagovi i greške.\n- Dodati su novi botovi.\n- Poruke bota imaju novi dizajn.\n- Smanjeno je vreme obrade operacija.\n- Notifikacije za mobilne aplikacije sada imaju novi izgled.\n\nHvala na pažnji! 😊**", inline=False)
+    embed.add_field(name="", value="**- Popravljeni su bagovi i greške.\n- Dodati su novi botovi.\n- Poruke bota imaju novi dizajn.\n- Smanjeno je vreme obrade operacija.\n- Notifikacije za mobilne aplikacije sada imaju novi izgled.\n\nHvala na pažnji! 😊**", inline=False)
     
     # Dodaj sliku na embed
     embed.set_image(url="attachment://sip-nova-obavestenja.png")
