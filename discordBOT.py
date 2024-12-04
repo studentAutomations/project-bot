5c3da2845b1ab7a300680ee25f1c161765c47144
studentAutomations <student.automations@gmail.com>
2024-12-04 18:09:45 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -1,8 +1,11 @@
 import os
-from dhooks import Webhook, Embed
+from dhooks import Webhook, Embed, File
 
 # Postavi URL za webhook
 WEBHOOK_URL = [os.getenv('PROJECTBOT')]
+
+# Putanja do slike
+image2_path = 'sip-nova-obavestenja.png'
 
 for url in WEBHOOK_URL:
     hook = Webhook(url)
@@ -15,11 +18,15 @@ for url in WEBHOOK_URL:
     )
     
     # Dodaj stavke u embed
-    embed.add_field(name="", value="- Popravljeni su bagovi i greške.", inline=False)
-    embed.add_field(name="", value="- Dodati su novi botovi.", inline=False)
-    embed.add_field(name="", value="- Poruke bota imaju novi dizajn.", inline=False)
-    embed.add_field(name="", value="- Smanjeno je vreme obrade operacija.", inline=False)
-    embed.add_field(name="", value="- Notifikacije za mobilne aplikacije sada imaju novi izgled.", inline=False)
+    embed.add_field(name="✅", value="Popravljeni su bagovi i greške.", inline=False)
+    embed.add_field(name="🤖", value="Dodati su novi botovi.", inline=False)
+    embed.add_field(name="✨", value="Poruke bota imaju novi dizajn.", inline=False)
+    embed.add_field(name="⏱️", value="Smanjeno je vreme obrade operacija.", inline=False)
+    embed.add_field(name="📱", value="Notifikacije za mobilne aplikacije sada imaju novi izgled.", inline=False)
     
-    # Pošalji embed
-    hook.send("@everyone", embed=embed)
+    # Dodaj sliku na embed
+    embed.set_image(url="attachment://sip-nova-obavestenja.png")
+    
+    # Pošalji poruku sa slikom
+    with File(image2_path, name="sip-nova-obavestenja.png") as file:
+        hook.send("@everyone", embed=embed, file=file)
